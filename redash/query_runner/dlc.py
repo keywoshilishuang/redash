9205428d039ba0bb591500d9c5d44c253725771c
stevensli <931400503@qq.com>
2021-03-16 16:46:15 +0800

    tencentcloud test

--- redash/query_runner/dlc.py
+++ redash/query_runner/dlc.py
@@ -2,9 +2,16 @@ import logging
 import re
 
 import requests
+import json
+import base64
 
 from redash.query_runner import *
 from redash.utils import json_dumps, json_loads
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.dlc.v20210125 import dlc_client, models
 
 logger = logging.getLogger(__name__)
 
@@ -16,25 +23,19 @@ class DLC(BaseSQLQueryRunner):
         return {
             "type": "object",
             "properties": {
-                "url": {
+                "Region": {
                     "type": "string",
-                    "default": "http://127.0.0.1:8123"
+                    "default": "ap-beijing"
                 },
-                "appid": {
-                    "type": "string",
-                    "default": "default"
+                "SecretId": {
+                    "type": "string"
                 },
-                "uin": {
+                "SecretKey": {
                     "type": "string"
                 },
                 "dbname": {
                     "type": "string",
                     "title": "Database Name"
-                },
-                "timeout": {
-                    "type": "number",
-                    "title": "Request Timeout",
-                    "default": 30
                 }
             },
             "required": ["dbname"],
@@ -54,7 +55,7 @@ class DLC(BaseSQLQueryRunner):
         return True
 
     def run_query(self, query, user):
-        logger.error("DLC is about to execute query: %s", query)
+        logger.error("DLC is about to execute query: %s user is:%s", query, user)
         logger.error("stevensli test DLC.")
         if query == "":
             json_data = None
@@ -63,6 +64,7 @@ class DLC(BaseSQLQueryRunner):
         try:
             # q = self._clickhouse_query(query)
             # data = json_dumps(q)
+            self._dlc_query(query, user)
             data = '{"rows": [{"name": "_temporary_and_external_tables"}, {"name": "default"}, {"name": "stevensli"}, {"name": "system"}], "columns": [{"type": "string", "friendly_name": "name", "name": "name"}]}'
             error = None
         except Exception as e:
@@ -70,5 +72,56 @@ class DLC(BaseSQLQueryRunner):
             logging.exception(e)
             error = unicode(e)
         return data, error
+
+    def _dlc_query(self, query, user):
+
+        dlcPioneer = dlc_executor(
+            self.configuration.get('SecretId', 'default'),
+            self.configuration.get('SecretKey', 'default'),
+            self.configuration.get('Region', 'default'),
+            self.configuration.get('dbname', 'default'),
+        )
+
+        try:
+            query = query[query.rindex("*/") + 2:]
+            query = query.strip()
+
+            sql = base64.b64encode(query)
+
+            task = {
+                "SQLTask":{
+                    "SQL":sql
+                }
+            }
+
+            req = models.CreateTaskRequest()
+            params = {
+                "DatabaseName": dlcPioneer.database,
+                "Task": task
+            }
+            req.from_json_string(json.dumps(params))
+
+            resp = dlcPioneer.client.CreateTask(req)
+            print(resp.to_json_string())
+
+        except TencentCloudSDKException as err:
+            print(err)
+
+
+class dlc_executor:
+    def __init__(self, id, key, region, db):
+        self.cred = credential.Credential(str(id), str(key))
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "dlc.tencentcloudapi.com"
+
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        self.client = dlc_client.DlcClient(self.cred, region, clientProfile)
+        self.database = db
+
+    def createTask(self, query, user):
+        req = models.CreateTaskRequest()
+        # params
+
 
 register(DLC)
